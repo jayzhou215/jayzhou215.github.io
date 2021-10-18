338108f793289140f68ae19915c04036ad53fdad
zhoujiejay <zhoujiejay@didiglobal.com>
2021-10-18 19:16:59 +0800

    最长子串

--- go_study/leecode/longestsubstr_test.go
+++ go_study/leecode/longestsubstr_test.go
@@ -1,7 +1,6 @@
 package leecode
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -40,41 +39,73 @@ func TestLongestSubStrWithoutRepeatChar(t *testing.T) {
 	}
 }
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+//执行用时：8 ms, 在所有 Go 提交中击败了70.39%的用户
+//内存消耗：2.9 MB, 在所有 Go 提交中击败了55.02%的用户
+func lengthOfLongestSubstring(str string) int {
+	var chars = make(map[byte]int, 26)
+	longestSubStringLength := 0
+	start := 0
+	byteList := []byte(str)
+	for i := 0; i < len(byteList); i++ {
+		curByte := byteList[i]
+		lastCharIdx, ok := chars[curByte]
+		if !ok {
+			longestSubStringLength = max(longestSubStringLength, i-start+1)
+			chars[curByte] = i
+		} else {
+			// 已经存在，需要重置start到重复字符串索引的后一位
+			for j := start; j <= lastCharIdx; j++ {
+				delete(chars, byteList[j])
+			}
+			start = lastCharIdx + 1
+			chars[curByte] = i
+		}
+	}
+	return longestSubStringLength
+}
+
 // Runtime: 520 ms, faster than 5.04% of Go online submissions for Longest Substring Without Repeating Characters.
 // Memory Usage: 8.1 MB, less than 6.10% of Go online submissions for Longest Substring Without Repeating Characters.
-func lengthOfLongestSubstring(str string) int {
-	substr := ""
-	tmpSubstr := ""
-	start := 0
-	curIdx := 0
-	curChars := make(map[string]interface{})
-	// 1. 检查重复char
-	// 2. 如果发现重复char，需考虑重复后起始位置
-	for curIdx < len(str) {
-		curChar := string(str[curIdx])
-		if _, ok := curChars[curChar]; ok {
-			curChars = make(map[string]interface{})
-			for j := 0; j < len(tmpSubstr); j++ {
-				if string(tmpSubstr[j]) == curChar {
-					start += j
-				}
-			}
-			start++
-			curIdx = start
-			curChars[string(str[curIdx])] = nil
-			tmpSubstr = str[start : curIdx+1]
-			curIdx++
-		} else {
-			curChars[curChar] = nil
-			tmpSubstr = str[start : curIdx+1]
-			curIdx++
-		}
-
-		if len(tmpSubstr) > len(substr) {
-			substr = tmpSubstr
-		}
-		fmt.Println(curChars, string(curChar), start, curIdx, tmpSubstr, substr)
-
-	}
-	return len(substr)
-}
+//func lengthOfLongestSubstring(str string) int {
+//	substr := ""
+//	tmpSubstr := ""
+//	start := 0
+//	curIdx := 0
+//	curChars := make(map[string]interface{})
+//	// 1. 检查重复char
+//	// 2. 如果发现重复char，需考虑重复后起始位置
+//	for curIdx < len(str) {
+//		curChar := string(str[curIdx])
+//		if _, ok := curChars[curChar]; ok {
+//			curChars = make(map[string]interface{})
+//			for j := 0; j < len(tmpSubstr); j++ {
+//				if string(tmpSubstr[j]) == curChar {
+//					start += j
+//				}
+//			}
+//			start++
+//			curIdx = start
+//			curChars[string(str[curIdx])] = nil
+//			tmpSubstr = str[start : curIdx+1]
+//			curIdx++
+//		} else {
+//			curChars[curChar] = nil
+//			tmpSubstr = str[start : curIdx+1]
+//			curIdx++
+//		}
+//
+//		if len(tmpSubstr) > len(substr) {
+//			substr = tmpSubstr
+//		}
+//		fmt.Println(curChars, string(curChar), start, curIdx, tmpSubstr, substr)
+//
+//	}
+//	return len(substr)
+//}
