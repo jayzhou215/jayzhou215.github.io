16886aa1b27e6b3fe789538444f66833b933e797
zhoujiejay <zhoujiejay@didiglobal.com>
2021-10-21 15:22:51 +0800

    add lock

--- go_study/tests/nplusplus_test.go
+++ go_study/tests/nplusplus_test.go
@@ -39,3 +39,22 @@ func Test_nPlusPlusRight(t *testing.T) {
 		})
 	}
 }
+
+func Test_nPlusPlusRight2(t *testing.T) {
+	tests := []struct {
+		name string
+		want int64
+	}{
+		{
+			name: "n++ right",
+			want: 200000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nPlusPlusRight2(); got != tt.want {
+				t.Errorf("nPlusPlus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- go_study/tests/nplusplusright.go
+++ go_study/tests/nplusplusright.go
@@ -24,3 +24,28 @@ func nPlusPlusRight() int64 {
 	wg.Wait()
 	return n
 }
+
+func nPlusPlusRight2() int64 {
+	var wg sync.WaitGroup
+	var n int64
+	wg.Add(2)
+	mu := sync.Mutex{}
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100000; i++ {
+			mu.Lock()
+			n++
+			mu.Unlock()
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100000; i++ {
+			mu.Lock()
+			n++
+			mu.Unlock()
+		}
+	}()
+	wg.Wait()
+	return n
+}
